a2b05ff52c5ce038f962ee7a32e73bdd6f3fef2e
Tony Costanzo <tonycos92@gmail.com>
2024-04-04 06:39:29 -0500

    deps: seo kit

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
-    <NuxtWelcome />..
+	  <p class="bg-red-500 text-white px-4 py-2 rounded-lg">
+		  Hello World
+	  </p>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxt/ui'
+    '@nuxt/ui',
+    '@nuxtjs/seo'
   ],
+  site: {
+    url: 'https://openrs2.netlify.app',
+    name: 'OpenRS2 Archive',
+    description: 'The OpenRS2 Archive is a collection of caches and XTEA keys for all versions of RuneScape that use JS5, and its precursor, to manage assets (mainline RuneScape builds 234 and greater, and all builds of Old School RuneScape).',
+    defaultLocale: 'en', // not needed if you have @nuxtjs/i18n installed
+  },
   app: {
     head: {
       titleTemplate: '%s %separator %siteName',
